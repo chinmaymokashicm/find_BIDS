57f3fa9968c77e7ee09ccb6f7b2aa4815099e8e8
Mokashi,Chinmay <CMokashi@mdanderson.org>
2026-02-13 16:45:18 -0600

    Update test paths to rsrch5 home
    Replace /Volumes/... paths with /rsrch5/home/... in test_generate_features.py so dicom_root and features_root point to the new rsrch5 mount locations. No other logic changes were made.

--- test_generate_features.py
+++ test_generate_features.py
@@ -3,9 +3,9 @@ from src.find_bids.models.extract.dataset import Dataset
 
 from pathlib import Path
 
-dicom_root: Path = Path("/Volumes/csi/Quarles_Lab/PROACTIVE_De-identidied")
+dicom_root: Path = Path("/rsrch5/home/csi/Quarles_Lab/PROACTIVE_De-identidied")
 
-features_root: Path = Path("/Volumes/csi/cmokashi/find_BIDS/features/PROACTIVE")
+features_root: Path = Path("/rsrch5/home/csi/cmokashi/find_BIDS/features/PROACTIVE")
 dataset: Dataset = Dataset.from_dir_with_session_level(dicom_root, features_root=features_root, dtype="DICOM", series_subdir_path="DICOM/")
 
 dataset.generate_features()
