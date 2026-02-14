216313329271144cb8ee31cc66c1ed2293961fa2
Mokashi,Chinmay <CMokashi@mdanderson.org>
2026-02-13 20:16:30 -0600

    Update test_generate_features.py

--- test_generate_features.py
+++ test_generate_features.py
@@ -8,4 +8,7 @@ dicom_root: Path = Path("/rsrch5/home/csi/Quarles_Lab/PROACTIVE_De-identidied")
 features_root: Path = Path("/rsrch5/home/csi/Quarles_Lab/find_BIDS/features/PROACTIVE")
 dataset: Dataset = Dataset.from_dir_with_session_level(dicom_root, features_root=features_root, dtype="DICOM", series_subdir_path="DICOM/")
 
+dataset.generate_bids_ids(replace_existing=True)
+dataset.export()
+
 dataset.generate_features()
